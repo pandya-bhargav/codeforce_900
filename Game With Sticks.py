75029d350e66bbb57496f230f5e2529dfdbe3bf4
pandya-bhargav <118329636+pandya-bhargav@users.noreply.github.com>
2022-11-16 22:27:04 +0530

    Update Game With Sticks.py

--- Game With Sticks.py
+++ Game With Sticks.py
@@ -1,23 +1,6 @@
-import sys
-sys.stdout = open('output.txt' , 'w')
-sys.stdin = open('input.txt' , 'r')
 n , m = input().split()
 n = int(n)
 m = int(m)
-# count = 0
-# inter_sec = n*m
-# while inter_sec!=0:
-#     n = n-1
-#     m = m-1
-#     inter_sec = n*m
-#     count+=1
-# if count%2 == 0:
-#     print("Malvika")
-# else:
-#     print("Akshat")
-
-# other
-
 if n>m :
     count = m
 else:
